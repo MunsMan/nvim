9b4a0b4536e0225a486e054024b546184c63a7b4
MunsMan <munsman.github@gmail.com>
2024-07-22 15:01:14 +0200

    renaming nil file

--- lua/plugins/lsp/lsps/nil.lua
+++
@@ -1,6 +0,0 @@
-return {
-    opts = {},
-    setup = function (opts)
-        require('lspconfig').nil_ls.setup(opts)
-    end
-}
---
+++ lua/plugins/lsp/lsps/nil_ls.lua
@@ -0,0 +1,6 @@
+return {
+    opts = {},
+    setup = function(opts)
+        require("lspconfig").nil_ls.setup(opts)
+    end,
+}
